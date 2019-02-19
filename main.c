50ffa6eec8fbf90b8a961446e6a45ebc536246ea
jacob <jtmarshall96@gmail.com>
2019-02-19 17:42:19 -0500

    This will break my cmputer

--- main.c
+++ main.c
@@ -5,22 +5,57 @@
 void *print_message_function( void *ptr );
 
 /*
- TODO: make a function to check that each column contains the digits 1 through 9
- TODO: A thread to check that each row contains the digits 1 through 9
+ TODO: make thread for valid_collumn
+ TODO: A thread for valid_row
  TODO: Nine threads to check that each of the 3*3 subgrids contains the digits 1 through 9 
+
 */
 
-int board[9][9]; 
+#define TRUE  1
+#define FALSE 0
 
-main()
+int board[9][9] = {6,2,4,5,3,9,1,8,7,
+		   5,1,9,7,2,8,6,3,4,
+		   8,3,7,6,1,4,2,9,5,
+		   1,4,3,8,6,5,7,2,9,
+		   9,5,8,2,4,7,3,6,1,
+		   7,6,2,3,9,1,4,5,8,
+		   3,7,1,9,5,6,8,4,2,
+		   4,9,6,1,8,2,5,7,3,
+		   2,8,5,4,7,3,9,1,6};
+
+
+int valid_collumn(int board[9][9]);
+
+int main()
 {
+	printf("collumn returns: %d\n",valid_collumn(board));
     
      exit(0);
 }
 
-void *print_message_function( void *ptr )
-{
-     char *message;
-     message = (char *) ptr;
-     printf("%s \n", message);
+
+int valid_collumn(int board[9][9]){
+
+	int row,collumn,number,i;
+	int nums[9];
+
+	for(row = 0; row<9;row++){
+		
+		for(i = 0; i<9;i++){
+		nums[i] = FALSE;
+		}
+
+		for(collumn =0;collumn<9;collumn++){
+			number = board[collumn][row];
+			printf("number: %d\n",number);
+			if(nums[number-1]){
+				return FALSE;
+			}
+			else{
+				nums[number-1] = 1;
+			}	
+		}
+	}
+	return TRUE;
 }
